5081a3ed6c50c504f2ff0973f9ce7803eb1635b8
Anand S <anandsr.dev@gmail.com>
2021-12-15 11:29:07 +0530

    first commit - day 1 completed

---
+++ day 1 - drum beats/script.js
@@ -0,0 +1,21 @@
+const playSound = (e) => {
+
+    const audio = document.querySelector(`audio[data-key = "${e.keyCode}"]`)
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`)
+    if (!audio)
+        return
+
+    key.classList.add('playing')
+    audio.currentTime = 0
+    audio.play()
+}
+
+const revert = (e) => {
+    if (e.propertyName !== 'transform') return
+    e.target.classList.remove('playing')
+}
+
+window.addEventListener('keydown', playSound)
+
+const keys = document.querySelectorAll('.key')
+keys.forEach(key => key.addEventListener('transitionend', revert))
